c9a585110e0628fe484b529f81affb25c06476c1
David Groff <cooldaveg73@gmail.com>
2011-10-22 09:10:24 -0700

    Added a migration to rescue message data

--- db/migrate/20111022155343_alter_messages_part_two.rb
+++ db/migrate/20111022155343_alter_messages_part_two.rb
@@ -1,24 +1,29 @@
 class AlterMessagesPartTwo < ActiveRecord::Migration
+
+  def self.find_person(mobile)
+    doctor = Doctor.find_by_mobile(mobile)
+    return doctor unless doctor.nil?
+    vhd = Vhd.find_by_mobile(mobile)
+    return vhd unless vhd.nil?
+    pm = Pm.find_by_mobile(mobile)
+    return pm unless pm.nil?
+    return nil
+  end
+
   def self.up
     Message.all.each do |m|
       if m.to_person_id.nil?
-        Message.to_person = find_person(m.to_number)
+        m.to_person = find_person(m.to_number)
       end
 
       if m.from_person_id.nil?
-        Message.from_person = find_person(m.from_number)
+        m.from_person = find_person(m.from_number)
       end
-      Message.save
+      m.save
     end
   end
 
   def self.down
   end
 
-  def find_person(mobile)
-    return vhd if (vhd = Vhd.find_by_mobile(mobile))
-    return doctor if (doctor = Doctor.find_by_mobile(mobile))
-    return pm if (pm = Pm.find_by_mobile(mobile))
-    return nil
-  end
 end
