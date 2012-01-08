e236386298a361026c3dbd36d52719181af5e796
David Groff <dgroff@stanford.edu>
2012-01-08 15:33:34 -0800

    added an active to the query that pages pm's, the temp fix i.e. on Yashoda was to simply delete her off all paging schemes, but this will allow us to deactivate pms

--- app/models/pm.rb
+++ app/models/pm.rb
@@ -35,7 +35,7 @@ class Pm < ActiveRecord::Base
   # notify scheme types: [ "acc", "req", "fin", "alert" ]
   def self.notify(type, kase)
     kase.project.pms.each do |pm|
-      query = "pm_id = ? AND alert_type = ?", pm.id, type
+      query = "pm_id = ? AND alert_type = ? AND active = ?", pm.id, type, true
       unless kase.project.notify_schemes.where(*query).blank?
 	send_info = { :msg => kase.send("#{type}_for_pm"), :case => kase }
 	Message.send_to_person(pm, send_info)
