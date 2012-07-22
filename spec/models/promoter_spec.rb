78ef69f8a9b099cc63d47044dd1e069c6872055c
David Groff <dgroff@stanford.edu>
2012-07-22 12:54:39 -0700

    cosmetic changes to apply for a project page from Pooja and changed text in admin_notification

--- spec/models/promoter_spec.rb
+++ spec/models/promoter_spec.rb
@@ -12,11 +12,8 @@
 #  username     :string(24)
 #  created_at   :datetime
 #  updated_at   :datetime
-<<<<<<< HEAD
-=======
 #  key          :string(1024)
 #  project_id   :integer(4)
->>>>>>> master
 #
 
 require 'spec_helper'
